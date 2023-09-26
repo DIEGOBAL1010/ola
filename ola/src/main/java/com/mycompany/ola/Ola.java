527f6dccec87d10e2d6792e88c1cd5c3514036e6
DIEGOBAL1010 <145385058+DIEGOBAL1010@users.noreply.github.com>
2023-09-26 09:20:01 -0600

    Add files via upload

---
+++ ola/src/main/java/com/mycompany/ola/Ola.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.ola;
+public class Ola {
+    public static void main(String[] args) {
+        System.out.println("ola periano");
+        System.out.println("soy diego");
+    }
+}
